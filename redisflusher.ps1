f07b2b45088a4a1ef5c8e250e890a3f5261eaf08
udhanMti <udhan@wso2.com>
2021-10-06 23:31:18 +0530

    first commit

---
+++ redisflusher.ps1
@@ -0,0 +1,25 @@
+param (
+         
+        [Parameter(Mandatory=$true)]
+        [string] 
+        $RGName,
+
+        [Parameter(Mandatory=$true)]
+        [string] 
+        $CacheName,
+
+        [parameter(Mandatory=$true)]
+        [PSCredential] $Creds
+
+        
+)
+
+$CacheUrl = $CacheName + ".redis.cache.windows.net"
+$AccessKey = $Creds.GetNetworkCredential().Password
+
+Connect-AzAccount -Identity -ErrorAction stop -WarningAction SilentlyContinue | Out-Null
+
+Set-AzRedisCache -ResourceGroupName $RGName -Name $CacheName -EnableNonSslPort $true
+Invoke-Command -ScriptBlock { cd "C:\Program Files\Redis" }
+Invoke-Command -ScriptBlock { .\redis-cli.exe -h $CacheUrl -p 6379 -a $AccessKey flushall }
+Set-AzRedisCache -ResourceGroupName $RGName -Name $CacheName -EnableNonSslPort $false
